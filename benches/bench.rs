889e272661b6cac02f37d86feb6fc2439245f179
Eshed Magali <esheder@gmail.com>
2024-12-31 10:54:10 +0200

    Added direct benchmark for the c extern functions

--- benches/bench.rs
+++ benches/bench.rs
@@ -2,7 +2,9 @@
 
 extern crate test;
 
-use logcosh::{approx_clogcosh, approx_logcosh, clogcosh, logcosh};
+use logcosh::{
+    approx_clogcosh, approx_logcosh, approx_logcosh_c32, clogcosh, logcosh, logcosh_c32,
+};
 use num::complex::Complex;
 use test::{black_box, Bencher};
 
@@ -75,4 +77,40 @@ fn bench_clogcosh_approx_mid(b: &mut Bencher) {
 fn bench_clogcosh_approx_high(b: &mut Bencher) {
     let x: Complex<f32> = Complex::new(1e3, 1e2);
     b.iter(|| black_box(approx_clogcosh(x)));
+}
+
+#[bench]
+fn bench_logcosh_c32_low(b: &mut Bencher) {
+    let x: Complex<f32> = Complex::new(1e-5, 2e-6);
+    b.iter(|| black_box(logcosh_c32(x)));
+}
+
+#[bench]
+fn bench_logcosh_c32_mid(b: &mut Bencher) {
+    let x: Complex<f32> = Complex::new(1.0, -1.0);
+    b.iter(|| black_box(logcosh_c32(x)));
+}
+
+#[bench]
+fn bench_logcosh_c32_high(b: &mut Bencher) {
+    let x: Complex<f32> = Complex::new(1e3, 1e2);
+    b.iter(|| black_box(logcosh_c32(x)));
+}
+
+#[bench]
+fn bench_approx_logcosh_c32_low(b: &mut Bencher) {
+    let x: Complex<f32> = Complex::new(1e-5, 2e-6);
+    b.iter(|| black_box(approx_logcosh_c32(x)));
+}
+
+#[bench]
+fn bench_approx_logcosh_c32_mid(b: &mut Bencher) {
+    let x: Complex<f32> = Complex::new(1.0, -1.0);
+    b.iter(|| black_box(approx_logcosh_c32(x)));
+}
+
+#[bench]
+fn bench_approx_logcosh_c32_high(b: &mut Bencher) {
+    let x: Complex<f32> = Complex::new(1e3, 1e2);
+    b.iter(|| black_box(approx_logcosh_c32(x)));
 }
